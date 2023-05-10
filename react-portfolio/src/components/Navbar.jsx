d49cc40da1f1f53a0270d00a8de2b94319f2a4ef
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-05-11 00:54:44 +0200

    created React project with Vite, made component classes and started on navbar

---
+++ react-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <header>
+            <ul className="nav-links">
+                <li className="nav-link">
+                    <a href="">About</a>
+                </li>
+                <li className="nav-link">
+                    <a href="">Projects</a>
+                </li>
+                <li className="nav-link">
+                    <a href="">Skills</a>
+                </li>
+                <li className="nav-link">
+                    <a href="">Contact</a>
+                </li>
+            </ul>
+        </header>
+    );
+}
