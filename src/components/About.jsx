8e2104c9e6dc68c764dbe1188d1505c230dc0fe5
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-05-11 03:23:27 +0200

    just added text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,1 +1,9 @@
-export default function About() {}
+import React from "react";
+
+export default function About() {
+    return(
+        <div className="hero">
+            <h1>I'm <b>Daniel </b> Robertson <br /> I love <b>Ani</b></h1>
+        </div>
+    );
+}
