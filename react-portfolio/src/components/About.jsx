31afa1784aadecf83998d8ca45e242440ae36465
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-08-21 15:00:59 +0200

    Deleted duplicate folder nested inside parent folder

--- react-portfolio/src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function About() {
-    return(
-        <div className="hero">
-            <h1>I'm <b>Daniel </b> Robertson <br /></h1>
-        </div>
-    );
-}
