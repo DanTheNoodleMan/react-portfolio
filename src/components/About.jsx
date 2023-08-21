b1dece3f63eb0417ed497f560650310dd967fbc5
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-08-22 01:42:16 +0200

    added photo and subtitle to vertical navbar, changed colors, still looking for color scheme

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
     return(
         <div className="hero">
-            <h1>I'm <b>Daniel </b> Robertson <br /> I love <b>Ani</b></h1>
+            <h1 className="shine">I'm <b>Daniel </b> Robertson <br /> I love <b>Ani</b></h1>
         </div>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "../assets/css/navbar.css";
+import "../../src/assets/sexy1.jpg";
+
 
 export default function Navbar() {
     return (
         <nav>
+            <div className="photo-section">
+                <img src="../../src/assets/sexy1.jpg" alt="profile" />
+                <h1 className="name shine">Daniel Robertson</h1>
+                <h3 className="position">Fullstack Developer</h3>
+            </div>
             <ul className="nav-links">
                 <li className="nav-link">
                     <a href="#about">About</a>
+                </li>
+                <li className="nav-link">
+                    <a href="#services">Services</a>
                 </li>
                 <li className="nav-link">
                     <a href="#projects">Projects</a>
@@ -17,6 +27,7 @@ export default function Navbar() {
                 <li className="nav-link">
                     <a href="#contact">Contact</a>
                 </li>
+                
             </ul>
         </nav>
     );
