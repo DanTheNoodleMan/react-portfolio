9a6972ce467484343b6612526eadb203405f4704
Daniel Robertson <31508473+DanTheNoodleMan@users.noreply.github.com>
2023-09-20 15:12:46 +0200

    Changed link for Bammbao project, wasnt changed before

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,9 +5,7 @@ import portfolio from "../assets/portfolio.png";
 import Card from "./Card";
 import useAnimateOnScroll from "./useAnimateOnScroll";
 
-
 export default function Projects() {
-
     const altBammbao = "Bammbao Wesbite Image";
     const altPortfolio = "Portfolio Website Image";
     const altPizza = "Pizza Game Website Image";
@@ -15,18 +13,39 @@ export default function Projects() {
     const portfolioTitle = "Portfolio";
     const bammbaoTitle = "Bammbao";
 
-    const portfolioDescription = "My portfolio website is as a showcase of my abilities with React. Throughout its development, I delved into React's intricate concepts and gained a solid grasp of its core principles. Hosted on Github Pages using Actions, this static website exhibits my skills and projects as well as reflects the valuable knowledge I acquired while using many different development tools. It exemplifies my ability to create responsive and optimized web solutions, catering seamlessly to both mobile and desktop users.";
-    
+    const portfolioDescription =
+        "My portfolio website is as a showcase of my abilities with React. Throughout its development, I delved into React's intricate concepts and gained a solid grasp of its core principles. Hosted on Github Pages using Actions, this static website exhibits my skills and projects as well as reflects the valuable knowledge I acquired while using many different development tools. It exemplifies my ability to create responsive and optimized web solutions, catering seamlessly to both mobile and desktop users.";
+
     const bammbaoDescription = (
         <span>
-            Bammbao is a website that I developed for a local restaurant featuring a dynamic menu powered by a RESTful API. The owner of the restaurant can dynamically change the contents of the menu with a secret url key I've given them. This project provided me with valuable experience handling intricate data integrations resulting in a seamlessly dynamic menu system, as well as developing a logical URI schema. With a mobile-first approach I honed my skills in optimizing user experiences across different devices, ensuring that the website performs well on both mobile and desktop.
-            <br /> <span className="highlight"><a href="#contact">(Contact me for the secret url key!)</a></span>
+            Bammbao is a website that I developed for a local restaurant
+            featuring a dynamic menu powered by a RESTful API. The owner of the
+            restaurant can dynamically change the contents of the menu with a
+            secret url key I've given them. This project provided me with
+            valuable experience handling intricate data integrations resulting
+            in a seamlessly dynamic menu system, as well as developing a logical
+            URI schema. With a mobile-first approach I honed my skills in
+            optimizing user experiences across different devices, ensuring that
+            the website performs well on both mobile and desktop.
+            <br />{" "}
+            <span className="highlight">
+                <a href="#contact">(Contact me for the secret url key!)</a>
+            </span>
         </span>
     );
 
-    const toolsPortfolio = [ "CSS", "React", "Javascript", "Vite" ];
-    const toolsBammbao = ["HTML", "CSS", "Bootstrap", "EJS", "Node.js", "Express.js", "MySQL", "Vite" ];
-    
+    const toolsPortfolio = ["CSS", "React", "Javascript", "Vite"];
+    const toolsBammbao = [
+        "HTML",
+        "CSS",
+        "Bootstrap",
+        "EJS",
+        "Node.js",
+        "Express.js",
+        "MySQL",
+        "Vite",
+    ];
+
     const linkPortfolio = "https://danthenoodleman.github.io/react-portfolio/";
     const linkBammbao = "https://bammbao.up.railway.app/";
 
@@ -40,8 +59,22 @@ export default function Projects() {
                     <span className="shine">Projects</span>
                 </h1>
                 <div className="project-grid">
-                    <Card imageSrc={portfolio} altD={altPortfolio} title={portfolioTitle} description={portfolioDescription} tools={toolsPortfolio} link={linkPortfolio} />
-                    <Card imageSrc={bammbao} altD={altBammbao} title={bammbaoTitle} description={bammbaoDescription} tools={toolsBammbao} link={linkBammbao}/>
+                    <Card
+                        imageSrc={portfolio}
+                        altD={altPortfolio}
+                        title={portfolioTitle}
+                        description={portfolioDescription}
+                        tools={toolsPortfolio}
+                        link={linkPortfolio}
+                    />
+                    <Card
+                        imageSrc={bammbao}
+                        altD={altBammbao}
+                        title={bammbaoTitle}
+                        description={bammbaoDescription}
+                        tools={toolsBammbao}
+                        link={linkBammbao}
+                    />
                     <Card altD={altPizza} />
                 </div>
             </div>
